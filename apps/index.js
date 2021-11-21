241eaeb4e82555f36a5209a5d062cd963ede8db6
darkun7 <182410101113@students.unej.ac.id>
2021-11-21 15:47:37 +0700

    Fix cors issue

--- apps/index.js
+++ apps/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const env = require('dotenv');
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
+app.use(cors())
 
 // View
 app.set('views', path.join(__dirname, 'app/views'));
