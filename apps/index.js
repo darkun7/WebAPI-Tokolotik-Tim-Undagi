a45644b1453b9df62f096b466398db1426039a83
darkun7 <182410101113@students.unej.ac.id>
2021-11-21 20:46:20 +0700

    Fix cors issue(2)

--- apps/index.js
+++ apps/index.js
@@ -4,7 +4,14 @@ const env = require('dotenv');
 const cors = require("cors");
 
 const app = express();
-app.use(cors())
+// app.use(cors())
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
 
 // View
 app.set('views', path.join(__dirname, 'app/views'));
